8e225d4677eef11bf25120b595c0613a3755517d
Michael LaDouceur <michaelladouceur1@gmail.com>
2022-08-23 00:11:04 -0500

    removed github preview to launch site on aws

--- src/App.js
+++ src/App.js
@@ -13,39 +13,30 @@ import Portfolio from "./components/Portfolio";
 import axios from "axios";
 
 function App() {
-  const { career, education, portfolio, skills } = data;
+	const { career, education, portfolio, skills } = data;
 
-  const [featureApp, setFeatureApp] = useState(portfolio[0]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [githubData, setGithubData] = useState({});
+	const [featureApp, setFeatureApp] = useState(portfolio[0]);
+	const [modalVisible, setModalVisible] = useState(false);
+	const [githubData, setGithubData] = useState({});
 
-  useEffect(async () => {
-    const {
-      data: { viewer },
-    } = await axios.get("/api/github");
-    setGithubData(viewer);
-  }, []);
+	// useEffect(async () => {
+	//   const {
+	//     data: { viewer },
+	//   } = await axios.get("/api/github");
+	//   setGithubData(viewer);
+	// }, []);
 
-  return (
-    <>
-      {modalVisible ? <></> : <SideNav />}
-      <FeatureAppModal
-        visible={modalVisible}
-        setVisible={setModalVisible}
-        featureApp={featureApp}
-        skills={skills}
-      />
-      <Profile githubData={githubData} />
-      <Portfolio
-        portfolio={portfolio}
-        setModalVisible={setModalVisible}
-        setFeatureApp={setFeatureApp}
-      />
-      <Skills skills={skills} />
-      <Career career={career} />
-      <Education education={education} />
-    </>
-  );
+	return (
+		<>
+			{modalVisible ? <></> : <SideNav />}
+			<FeatureAppModal visible={modalVisible} setVisible={setModalVisible} featureApp={featureApp} skills={skills} />
+			<Profile githubData={githubData} />
+			<Portfolio portfolio={portfolio} setModalVisible={setModalVisible} setFeatureApp={setFeatureApp} />
+			<Skills skills={skills} />
+			<Career career={career} />
+			<Education education={education} />
+		</>
+	);
 }
 
 export default App;
