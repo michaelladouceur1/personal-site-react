98f60df34bd1725756ac320dbc9e7eb93807666b
michael-aris <91093910+michael-aris@users.noreply.github.com>
2023-03-09 18:56:45 -0600

    formatting for profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,53 +8,58 @@ import GithubPreview from "./GithubPreview/GithubPreview";
 import "./css/Profile.scss";
 
 function Profile(props) {
-	// const { githubData } = props;
+  // const { githubData } = props;
 
-	function getAge() {
-		const birthday = new Date("06/09/1992");
-		const today = new Date();
-		const diff = new Date(today - birthday);
-		return Math.abs(diff.getUTCFullYear() - 1970);
-	}
+  function getAge() {
+    const birthday = new Date("06/09/1992");
+    const today = new Date();
+    const diff = new Date(today - birthday);
+    return Math.abs(diff.getUTCFullYear() - 1970);
+  }
 
-	return (
-		<Section name="profile">
-			<div className="section-header">
-				<h1>Michael LaDouceur</h1>
-				<h3>A former mechanical engineer finding his passion in software development</h3>
-			</div>
-			<div className="section-body">
-				<div className="profile">
-					<div className="profile-content">
-						<p>
-							I am a full stack software developer with an appetite for learning. In my free time I enjoy travelling, wood working, exercise, camping, and
-							anything science & design related. Also enjoy taking pictures with sqirrels.
-						</p>
-					</div>
-					<img src="./assets/portrait-2.JPG" alt="" />
-					<div className="profile-content">
-						<div>
-							<h3>Name:</h3>
-							<h4>Michael LaDouceur</h4>
-						</div>
-						<div>
-							<h3>Age:</h3>
-							<h4>{getAge()}</h4>
-						</div>
-						<div>
-							<h3>Location:</h3>
-							<h4>Chicago, IL</h4>
-						</div>
-						<div>
-							<h3>Favorite Food:</h3>
-							<h4>Sushi</h4>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* <GithubPreview githubData={githubData} /> */}
-		</Section>
-	);
+  return (
+    <Section name="profile">
+      <div className="section-header">
+        <h1>Michael LaDouceur</h1>
+        <h3>
+          A software engineer with experience and passion for front-end
+          technologies and robotics
+        </h3>
+      </div>
+      <div className="section-body">
+        <div className="profile">
+          <div className="profile-content">
+            <p>
+              I am a full stack software developer with an appetite for
+              learning. In my free time I enjoy travelling, wood working,
+              exercise, camping, and anything science & design related. Also
+              enjoy taking pictures with sqirrels.
+            </p>
+          </div>
+          <img src="./assets/portrait-2.JPG" alt="" />
+          <div className="profile-content">
+            <div>
+              <h3>Name:</h3>
+              <h4>Michael LaDouceur</h4>
+            </div>
+            <div>
+              <h3>Age:</h3>
+              <h4>{getAge()}</h4>
+            </div>
+            <div>
+              <h3>Location:</h3>
+              <h4>Chicago, IL</h4>
+            </div>
+            <div>
+              <h3>Favorite Food:</h3>
+              <h4>Sushi</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <GithubPreview githubData={githubData} /> */}
+    </Section>
+  );
 }
 
 export default Profile;
